cf73b5d9227aea93814f367b5a3ad564ea7f8b5d
tahanialhammad <tahani_alhammad@yahoo.com>
2024-12-27 13:47:26 +0100

    update and run build

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -2,22 +2,13 @@
 defineProps({
     type: {
         type: String,
-        default: 'button',
-    },
-    href: {
-        type: String,
-        default: null, // Default to null, so it can render a button if no href is provided
+        default: 'submit',
     },
 });
 </script>
 
 <template>
-    <a v-if="href" :href="href"
-        class="inline-flex items-center px-4 py-2 bg-rose-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-rose-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
-        <slot />
-    </a>
-    <button v-else :type="type"
-        class="inline-flex items-center px-4 py-2 bg-rose-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-rose-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-rose-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-rose-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
         <slot />
     </button>
 </template>
--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -1,21 +1,42 @@
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-</script>
-
 <template>
     <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                user Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                   
-                </div>
-            </div>
+        <Container>
+            <template #header>
+                <h2 class="font-semibold text-xl text-slate-00 leading-tight">
+                    User Dashboard
+                </h2>
+            </template>
+            <div class="relative mb-8 flex justify-between w-full">
+            <h1 class="relative font-black text-4xl z-10 uppercase">
+                My Posts
+            </h1>
+            <GradientCircle class="absolute -top-8 -left-10 z-0" />
+            <Link  :href="route('posts.create')"  class="font-black text-3xl rounded-full">+</Link>
         </div>
+
+            <!-- Check if myPosts is not an empty array -->
+            <div v-if="myPosts && myPosts.length > 0">
+                <MyPostsList :myPosts="myPosts" />    
+            </div>
+
+            <div v-else>
+                Add your first post
+            </div>
+        </Container>
     </AppLayout>
 </template>
+
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Container from '@/Components/Container.vue';
+import MyPostsList from './Partials/MyPostsList.vue';
+import GradientCircle from "@/Components/Svg/GradientCircle.vue";
+import { Link } from "@inertiajs/vue3";
+
+const props = defineProps({
+    myPosts: {
+        type: Array,
+        default: () => [],
+    }
+});
+</script>
