8bfc7b345781d26ff39adb4a5c0f4ff36098acf9
tahanialhammad <tahani_alhammad@yahoo.com>
2025-01-07 12:50:48 +0100

    update and run build

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\PostResource;
 use App\Http\Resources\TopicResource;
 use App\Http\Resources\UserResource;
+use App\Models\Marketing;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -23,12 +24,12 @@ class DashboardController extends Controller
     {
         $users = UserResource::collection(User::paginate(10));
         $topics = TopicResource::collection(Topic::all());
-        
+        $marketings = Marketing::all();
         $authUser = auth()->user();
 
         $myPosts = PostResource::collection( $authUser->posts()->latest()->get());
 
-        return inertia('Admin/Dashboard', compact('users', 'topics', 'myPosts'));
+        return inertia('Admin/Dashboard', compact('users', 'topics', 'myPosts', 'marketings'));
     }
 
     private function showUserDashboard()
